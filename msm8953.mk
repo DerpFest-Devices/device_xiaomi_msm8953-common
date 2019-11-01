35f58811f404595aad111706ee674c7fc2bd3fd3
Dan Pasanen <dan.pasanen@gmail.com>
2019-11-01 07:19:07 -0500

    Revert "msm8953-common: Exclude lineage-sdk overlays from RRO"
    This reverts commit 7e5a40256c69cbf818b793cf77bef86f615466f2.
    
    Change-Id: I2bcc0dd6ebf063a62d14ffbae281f41d0b02ed29

--- msm8953.mk
+++ msm8953.mk
@@ -19,7 +19,7 @@ $(call inherit-product, vendor/xiaomi/msm8953-common/msm8953-common-vendor.mk)
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
-
+PRODUCT_ENFORCE_RRO_TARGETS := *
 
 # Board
 PRODUCT_USES_QCOM_HARDWARE := true
