a62489a0c797000a275a43af7c44c2952c496723
reharsh67 <reharshdeshpande@gmail.com>
2021-05-16 09:37:49 +0000

    Revert "msm8953-common: Enable AOSP surfaceflinger"
    This reverts commit f8cb66fbfcb38e856dbdcd59725ae9d19da4801a.

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -175,9 +175,6 @@ endif
 include device/qcom/sepolicy-legacy-um/Sepolicy.mk
 BOARD_SEPOLICY_DIRS += $(VENDOR_PATH)/sepolicy
 
-# SurfaceFlinger
-TARGET_USE_AOSP_SURFACEFLINGER := true
-
 # Wi-Fi
 BOARD_HAS_QCOM_WLAN := true
 BOARD_HOSTAPD_DRIVER := NL80211
